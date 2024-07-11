d6cfa3a32faa345e2dd538402bfc694c2b12bd29
Антон Куканов <motokyka@yandex.ru>
2024-07-12 00:46:12 +0300

    first commit

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,49 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    # parametrized_client - название параметра,
+    # в который будут передаваться фикстуры;
+    # Параметр expected_status - ожидаемый статус ответа.
+    'parametrized_client, expected_status',
+    # В кортеже с кортежами передаём значения для параметров:
+    (
+            ('not_author_client', HTTPStatus.NOT_FOUND),
+            ('author_client', HTTPStatus.OK)
+    ),
+)
+# Этот декоратор оставляем таким же, как в предыдущем тесте.
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+# В параметры теста добавляем имена parametrized_client и expected_status.
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    # Делаем запрос от имени клиента parametrized_client:
+    response = parametrized_client.get(url)
+    # Ожидаем ответ страницы, указанный в expected_status:
+    assert response.status_code == expected_status
