081fca3f6d42b5a3dee309801f99dfb5d768104e
Антон Куканов <motokyka@yandex.ru>
2024-07-17 21:17:33 +0300

    final commit

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 import pytest
 from django.urls import reverse
 
+from pytest_django.asserts import assertRedirects
+
 
 # Указываем в фикстурах встроенный клиент.
 def test_home_availability_for_anonymous_user(client):
@@ -23,27 +25,42 @@ def test_pages_availability_for_author(author_client, name, note):
 
 
 @pytest.mark.parametrize(
-    # parametrized_client - название параметра,
-    # в который будут передаваться фикстуры;
-    # Параметр expected_status - ожидаемый статус ответа.
     'parametrized_client, expected_status',
-    # В кортеже с кортежами передаём значения для параметров:
+    # Предварительно оборачиваем имена фикстур
+    # в вызов функции pytest.lazy_fixture().
     (
-            ('not_author_client', HTTPStatus.NOT_FOUND),
-            ('author_client', HTTPStatus.OK)
+            (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+            (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
     ),
 )
-# Этот декоратор оставляем таким же, как в предыдущем тесте.
 @pytest.mark.parametrize(
     'name',
     ('notes:detail', 'notes:edit', 'notes:delete'),
 )
-# В параметры теста добавляем имена parametrized_client и expected_status.
 def test_pages_availability_for_different_users(
         parametrized_client, name, note, expected_status
 ):
     url = reverse(name, args=(note.slug,))
-    # Делаем запрос от имени клиента parametrized_client:
     response = parametrized_client.get(url)
-    # Ожидаем ответ страницы, указанный в expected_status:
     assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+            ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+            ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+            ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+            ('notes:add', None),
+            ('notes:success', None),
+            ('notes:list', None),
+    ),
+)
+# Передаём в тест анонимный клиент, name проверяемых страниц и args:
+def test_redirects(client, name, args):
+    login_url = reverse('users:login')
+    # Теперь не надо писать никаких if и можно обойтись одним выражением.
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
